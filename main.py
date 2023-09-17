eb091e397ff7cc52f9f43448a20f7f42c0584ba6
Metrohan <metehangunen34@outlook.com>
2023-09-17 20:20:44 +0300

    Record location changed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pyautogui
 import cv2
 import numpy as np
 import sys
+import os.path
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtWidgets
@@ -10,7 +11,7 @@ from PyQt5.QtWidgets import *
 import recorderUI
 
 
-class recording(QtWidgets.QMainWindow, recorderUI.Ui_MainWindow):
+class recordingGUI(QtWidgets.QMainWindow, recorderUI.Ui_MainWindow):
     def __init__(self):
         super().__init__()
 
@@ -18,37 +19,32 @@ class recording(QtWidgets.QMainWindow, recorderUI.Ui_MainWindow):
         self.show()
 
         self.initVideoWriter()
+        self.createFolder()
+        self.recordLocation()
 
-        self.initSettings()
-
-        self.comboBox.currentIndexChanged.connect(self.updateSettings)
-        self.comboBox_2.currentIndexChanged.connect(self.updateSettings)
-        self.comboBox_3.currentIndexChanged.connect(self.updateSettings)
-        self.comboBox_4.currentIndexChanged.connect(self.updateSettings)
-        self.lineEdit.textChanged.connect(self.updateSettings)
+        self.comboBox.currentIndexChanged.connect(self.initVideoWriter)
+        self.lineEdit.textEdited.connect(self.initVideoWriter)
 
         self.pushButton.pressed.connect(self.monitorScreen)
+        self.pushButton_2.pressed.connect(self.stopRecording)
 
-    def initSettings(self):
+        global onLive
+        onLive = 0
 
-        self.comboBox.setCurrentIndex(13)
-        self.comboBox_2.setCurrentIndex(6)
-        self.comboBox_3.setCurrentIndex(1)
-        self.comboBox_4.setCurrentIndex(1)
+    def createFolder(self):
+        record_folder = os.path.expanduser("~\\Documents\\ScreenRecorder")
+        if not os.path.exists(record_folder):
+            os.mkdir(record_folder)
+
+    def recordLocation(self):
+        self.record_folder = os.path.expanduser("~\\Documents\\ScreenRecorder")
+        path = os.path.join(self.record_folder)
+        os.chdir(path)
 
     def initVideoWriter(self):
         prefRes = self.comboBox.currentText()  # preferred resolution
-        width, height = map(int, prefRes.split('x'))
-        prefCodec = self.comboBox_2.currentText()  # preferred codec
-        prefFormat = self.comboBox_4.currentText()  # preferred file format
-        prefName = self.lineEdit.text()
-
-        resolution = (width, height)
-        codec = cv2.VideoWriter_fourcc(*prefCodec)
-        filename = f"{prefName}" + prefFormat
-        fps = 30.0
-
-        self.out = cv2.VideoWriter(filename, codec, fps, resolution)
+        self.width, self.height = map(int, prefRes.split('x'))
+        self.prefName = self.lineEdit.text()
 
     def monitorScreen(self):
         cv2.namedWindow("Monitor", cv2.WINDOW_NORMAL)
@@ -56,31 +52,36 @@ class recording(QtWidgets.QMainWindow, recorderUI.Ui_MainWindow):
         self.recordScreen()
 
     def recordScreen(self):
-        while True:
+        self.onLive = 1
+        self.pushButton.hide()
+        self.pushButton_2.show()
+        resolution = (self.width, self.height)
+        codec = cv2.VideoWriter_fourcc(*'mp4v')
+        filename = f"{self.prefName}" + '.mp4'
+        fps = 30.0
+
+        self.out = cv2.VideoWriter(filename, codec, fps, resolution)
+        while self.onLive == 1:
             img = pyautogui.screenshot()
             frame = np.array(img)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             self.out.write(frame)
             cv2.imshow('Monitor', frame)
             if cv2.waitKey(1) == ord('p'):
+                self.onLive = 0
                 self.stopRecording()
                 break
 
     def stopRecording(self):
         self.out.release()
         cv2.destroyAllWindows()
-
-    def updateSettings(self):
-        new_name = self.lineEdit.text()
-        if new_name != self.prefName:
-            self.stopRecording()
-            self.initVideoWriter()
-            self.prefName = new_name
+        self.pushButton.show()
+        self.pushButton_2.hide()
 
 
 def main():
     app = QApplication(sys.argv)
-    main_widget = recording()
+    main_widget = recordingGUI()
     main_widget.show()
     sys.exit(app.exec_())
 
