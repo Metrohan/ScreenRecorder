08185e5286faabc5114a94b4e5091fd711bce4f8
Metrohan <metehangunen34@outlook.com>
2023-10-12 16:06:43 +0300

    tray is not supported anymore

--- tray.py
+++ tray.py
@@ -5,7 +5,7 @@ from PIL import Image
 from recording import *
 
 
-def systemTray(onLive):
+def systemTray(liveCheck):
     path = os.path.abspath(os.path.dirname(__file__) + "\\ico")
     ico_folder = os.path.expanduser(path)
 
@@ -13,10 +13,10 @@ def systemTray(onLive):
         if str(query) == "Exit":
             icon.stop()
 
-    if onLive:
+    if liveCheck:
         image = Image.open(os.path.join(ico_folder, "rec2button.png"))
         print("onLive true")
-    elif onLive == 0:
+    else:
         image = Image.open(os.path.join(ico_folder, "recbutton.png"))
         print("onLive false")
 
