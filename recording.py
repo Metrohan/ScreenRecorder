0e8be0bfc437b78d4a044e40bbbbdbffe3d521d8
Metrohan <metehangunen34@outlook.com>
2023-10-28 20:07:08 +0300

    add file loc explorer and init func

--- recording.py
+++ recording.py
@@ -4,7 +4,7 @@ import numpy as np
 import os.path
 import time
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QFileDialog
 from PyQt5.QtCore import QTimer
 
 
@@ -15,12 +15,27 @@ class Recording:
         self.timer = QTimer(self.main_window)
         self.timer.timeout.connect(self.updateTimer)
         self.out = None
+        self.check = 0
+        self.filename = ""
+        self.resolution = (640,480)
 
     def createFolder(self):
         record_folder = os.path.expanduser("~\\Documents\\Records")
         if not os.path.exists(record_folder):
             os.mkdir(record_folder)
 
+    def set_file_location(self):
+        self.folder_path = self.main_window.locLineEdit.text()
+
+    def save_file_location(self):
+        self.folder_path = self.main_window.locLineEdit.text()
+
+    def choose_file_location(self):
+        folder = QFileDialog.getExistingDirectory()
+        if folder:
+            self.main_window.locLineEdit.setText(folder)
+            self.folder_path = folder
+    
     def recordLocation(self):
         self.record_folder = os.path.expanduser("~\\Documents\\Records")
         path = os.path.join(self.record_folder)
@@ -29,17 +44,19 @@ class Recording:
     def initVideoWriter(self):
         prefRes = self.main_window.comboBox.currentText()
         self.width, self.height = map(int, prefRes.split('x'))
-        self.prefName = self.main_window.lineEdit.text()
+        self.prefName = self.main_window.nameEdit.text()
 
     def startRecording(self):
         self.startTimer = 0
         self.onLive = True
 
-        self.main_window.pushButton.hide()
-        self.main_window.pushButton_2.show()
+        self.main_window.startButton.hide()
+        self.main_window.stopButton.show()
 
         resolution = (self.width, self.height)
         codec = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+        if not self.prefName:
+            self.prefName = "Recording"
         filename = f"{self.prefName}" + '.avi'
 
         self.out = cv2.VideoWriter(filename, codec, 20.0, resolution)
@@ -59,24 +76,34 @@ class Recording:
         self.startTimer = 0
         self.onLive = False
 
-        self.main_window.label_2.setText("00:00:00")
+        self.main_window.timer.setText("00:00:00")
         self.timer.stop()
         self.out.release()
         cv2.destroyAllWindows()
 
-        self.main_window.pushButton.show()
-        self.main_window.pushButton_2.hide()
+        self.main_window.startButton.show()
+        self.main_window.stopButton.hide()
 
     def updateTimer(self):
         if self.onLive:
             self.startTimer += 1
-            self.main_window.label_2.setText(time.strftime("%H:%M:%S", time.gmtime(self.startTimer)))
+            self.main_window.timer.setText(time.strftime("%H:%M:%S", time.gmtime(self.startTimer)))
 
     def countdown(self):
         t = 5
         while t != -1:
-            self.main_window.label_3.setText(str(t))
+            self.main_window.countdown.setText(str(t))
             QApplication.processEvents()
             time.sleep(1)
             t -= 1
         self.startRecording()
+        
+
+    def checkChoices(self):
+        self.prefName = self.main_window.nameEdit.text()
+        
+
+        self.countdown()
+
+        
+
